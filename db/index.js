434405eaf324c0548d36805f9962ea3a50a69fac
BrandonKutivan <bskutivan@gmail.com>
2020-05-31 15:33:18 -0700

    updated msql connection to use .env for password protection

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'BSK',
+    password: process.env.DB_PASS,
     database: 'employee_tracker'
 });
 
